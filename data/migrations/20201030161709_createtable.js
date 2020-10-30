242f8a0e31e075363a2ee241c5aa282140be546a
JenVest2020 <jenvest2019@gmail.com>
2020-10-30 16:26:12 -0500

    migration completed and running

---
+++ data/migrations/20201030161709_createtable.js
@@ -0,0 +1,20 @@
+const { table } = require("console");
+
+exports.up = function (knex) {
+    return knex.schema
+        .createTable('employers', tbl => {
+            tbl.increments();
+            tbl.string('name&title', 128).notNullable();
+            tbl.string('company', 128).notNullable();
+            tbl.string('phone_number', 15).notNullable().unique();
+            tbl.string('email', 128).notNullable().unique();
+            tbl.string('comm&instructions');
+        })
+
+};
+
+exports.down = function (knex) {
+    return knex.schema
+        .dropTableIfExists('employers');
+
+};
